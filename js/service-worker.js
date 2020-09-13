a84e29f5f5f8245dde184ade89125ff528e2229b
daniel romero <raiga1234@gmail.com>
2020-09-13 15:44:54 -0500

    Fix service worker

--- js/service-worker.js
+++ js/service-worker.js
@@ -21,20 +21,25 @@
 const CACHE_NAME = 'static-cache-v1';
 
 // CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [];
+const FILES_TO_CACHE = ['index.html', 'js/index.1.js'];
 
 self.addEventListener('install', (evt) => {
-    console.log('[ServiceWorker] Install');
-    // CODELAB: Precache static resources here.
+    caches.open(CACHE_NAME).then((cache) => {
+        console.log('[ServiceWorker] Pre-caching offline page');
+        return cache.addAll(FILES_TO_CACHE);
+    });
 
-    self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
-    console.log('[ServiceWorker] Activate');
-    // CODELAB: Remove previous cached data from disk.
-
-    self.clients.claim();
+    caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+                console.log('[ServiceWorker] Removing old cache', key);
+                return caches.delete(key);
+            }
+        }));
+    })
 });
 
 self.addEventListener('fetch', (evt) => {
