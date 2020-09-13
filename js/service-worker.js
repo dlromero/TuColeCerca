23081572dd397a84ca6c1f28426444cda9d70718
daniel romero <raiga1234@gmail.com>
2020-09-13 16:04:30 -0500

    update activate, fetch and install Service Worker

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,37 +1,46 @@
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = ['index.html'];
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html'
+];
 
-// CODELAB: Precache static resources here.
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-    })
-);
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
 
-// CODELAB: Remove previous cached data from disk.
-evt.waitUntil(
-    caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-                console.log('[ServiceWorker] Removing old cache', key);
-                return caches.delete(key);
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
             }
-        }));
-    })
-);
+            return fetch(event.request);
+        })
+    );
+});
 
-// CODELAB: Add fetch event handler here.
-if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
-    return;
-}
-evt.respondWith(
-    fetch(evt.request)
-    .catch(() => {
-        return caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.match('offline.html');
-            });
-    })
-);
+self.addEventListener('activate', function(event) {
+
+    var cacheAllowlist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheAllowlist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
